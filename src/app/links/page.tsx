4fe205fb59e20acdf2fe804a068c1c308311c617
bavichugo <victorhugobatistela@gmail.com>
2024-02-12 11:00:30 -0500

    Fix styling for links page

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,7 +1,7 @@
 import fetchUrls from "@/actions/fetchUrls";
 import WhiteCard from "@/components/WhiteCard";
-import TextDisplay from "@/components/TextDisplay";
 import DeleteUrlButton from "./DeleteUrlButton";
+import React from "react";
 
 export default async function YourLinksPage() {
   const urls = await fetchUrls();
@@ -9,23 +9,41 @@ export default async function YourLinksPage() {
   return (
     <div className="mx-auto w-full max-w-4xl flex flex-col gap-4 p-4">
       {urls?.map((url) => (
-        <WhiteCard className="grid grid-cols-9 gap-4" key={url.short_url}>
-          <TextDisplay
-            containerClass="col-span-2"
-            label="Short"
-            text={url.short_url}
-            textClass="overflow-scroll whitespace-nowrap"
+        <WhiteCard className="flex flex-col" key={url.short_url}>
+          <div className="flex justify-between w-full">
+            <UrlDisplay
+              key={url.short_url}
+              label="Short URL: "
+              link={url.short_url}
+            />
+            <DeleteUrlButton />
+          </div>
+          <UrlDisplay
+            key={url.long_url}
+            label="Long URL: "
+            link={url.long_url}
           />
-          <TextDisplay
-            containerClass="col-span-6"
-            label="Long"
-            text={url.long_url}
-            textClass="overflow-scroll whitespace-nowrap"
-          />
-          <DeleteUrlButton />
         </WhiteCard>
       ))}
     </div>
   );
 }
 
+interface IUrlDisplay {
+  label: string;
+  link: string;
+}
+
+const UrlDisplay: React.FC<IUrlDisplay> = ({ label, link }) => {
+  return (
+    <div className="flex w-full">
+      <span className="mr-2 whitespace-nowrap">{label}</span>
+      <a
+        className="max-w-xl w-full overflow-scroll whitespace-nowrap underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+        href={link}
+      >
+        {link}
+      </a>
+    </div>
+  );
+};
