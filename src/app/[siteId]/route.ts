6e24fe704b44b155ee61463e4ed83ec3bb6f4309
bavichugo <victorhugobatistela@gmail.com>
2024-02-12 21:13:21 -0500

    Crete delete api route logic

--- src/app/[siteId]/route.ts
+++ src/app/[siteId]/route.ts
@@ -1,10 +1,27 @@
-// api > hello > route.ts
+import dbConnect from "@/lib/dbConnect";
+import UrlRecord from "@/models/url";
 import { NextRequest, NextResponse } from "next/server";
+
+interface IContext {
+  params: { siteId: string};
+}
 
 export async function GET(request: NextRequest) {
   return NextResponse.redirect("https://medium.com/@patel.d/quick-guide-add-get-api-in-next-js-13-app-router-69f6e5e938be");
 }
 
-export async function DELETE(request: NextRequest) {
-  return NextResponse.json("Success deleting route!");
+export async function DELETE(request: NextRequest, context: IContext) {
+  try {
+    await dbConnect();
+    const siteId = context.params.siteId;
+    const result = await UrlRecord.deleteOne({ short_url: siteId });
+
+    if (result.deletedCount === 0) {
+      return NextResponse.json({ message: "No record found to delete." }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: "Success deleting route!" }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Failed to delete URL record." }, { status: 500 });
+  }
 }
--- src/app/links/DeleteButton.tsx
+++ src/app/links/DeleteButton.tsx
@@ -13,6 +13,18 @@ const DeleteButton: React.FC<IDeleteButton> = ({ shortUrl }) => {
   const [showModal, setShowModal] = useState<boolean>(false);
   const modalRef = useRef<HTMLDialogElement>(null);
 
+  const onDeleteHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`/${shortUrl}`, {
+        method: "DELETE",
+      });
+
+    } catch (err) {
+      console.log();
+    }
+  };
+
   useEffect(() => {
     if (showModal) {
       modalRef.current?.showModal();
@@ -31,20 +43,26 @@ const DeleteButton: React.FC<IDeleteButton> = ({ shortUrl }) => {
       {showModal && (
         <dialog className="max-w-sm w-full" ref={modalRef}>
           <WhiteCard className="flex flex-col">
-            <span className="font-bold">Are you sure to delete this URL?</span>
-            <span className="">If you delete you cannot recover it</span>
-            <span className="m-auto my-4 font-bold">{shortUrl}</span>
-            <div className="flex ml-auto gap-2">
-              <Button
-                className="border hover:bg-black/10 rounded px-2 py-1"
-                onClick={() => {
-                  setShowModal(false);
-                }}
-              >
-                Cancel
-              </Button>
-              <Button color="orange">Delete</Button>
-            </div>
+            <form onSubmit={onDeleteHandler}>
+              <span className="font-bold">
+                Are you sure to delete this URL?
+              </span>
+              <span className="">If you delete you cannot recover it</span>
+              <span className="m-auto my-4 font-bold">{shortUrl}</span>
+              <div className="flex ml-auto gap-2">
+                <Button
+                  className="border hover:bg-black/10 rounded px-2 py-1"
+                  onClick={() => {
+                    setShowModal(false);
+                  }}
+                >
+                  Cancel
+                </Button>
+                <Button color="orange" type="submit">
+                  Delete
+                </Button>
+              </div>
+            </form>
           </WhiteCard>
         </dialog>
       )}
